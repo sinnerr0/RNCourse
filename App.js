65791dcb5843155afb416514ca9f564a5cee28bc
kyongsikC <kyeongsik.choi@greenlabs.co.kr>
2022-11-21 15:16:57 +0900

    feat: section13

--- App.js
+++ App.js
@@ -1,75 +1,34 @@
-import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import * as SplashScreen from "expo-splash-screen";
-
-import AllPlaces from "./screens/AllPlaces";
-import AddPlace from "./screens/AddPlace";
-import IconButton from "./components/UI/IconButton";
-import { Colors } from "./constants/colors";
-import Map from "./screens/Map";
-import { init } from "./util/database";
-import PlaceDetails from "./screens/PlaceDetails";
-
-SplashScreen.preventAutoHideAsync();
-
-const Stack = createNativeStackNavigator();
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
 
 export default function App() {
-  useEffect(() => {
-    init()
-      .then(async () => {
-        await SplashScreen.hideAsync();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
+  async function getUserLocationHandler() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+    console.log("location", location);
+  }
   return (
-    <>
-      <StatusBar style="dark" />
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: Colors.primary500 },
-            headerTintColor: Colors.gray700,
-            contentStyle: { backgroundColor: Colors.gray700 },
-          }}
-        >
-          <Stack.Screen
-            name="AllPlaces"
-            component={AllPlaces}
-            options={({ navigation }) => ({
-              title: "Your Favorite Places",
-              headerRight: ({ tintColor }) => (
-                <IconButton
-                  icon="add"
-                  size={24}
-                  color={tintColor}
-                  onPress={() => navigation.navigate("AddPlace")}
-                />
-              ),
-            })}
-          />
-          <Stack.Screen
-            name="AddPlace"
-            component={AddPlace}
-            options={{
-              title: "Add a new Place",
-            }}
-          />
-          <Stack.Screen name="Map" component={Map} />
-          <Stack.Screen
-            name="PlaceDetails"
-            component={PlaceDetails}
-            options={{
-              title: "Loading Place...",
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
+    <View style={styles.container}>
+      <Button title="Get Location" onPress={getUserLocationHandler} />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
